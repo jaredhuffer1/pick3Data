112fa02cbc673160cea3e403013ecc7903635021
jaredhuffer1 <jaredhuffer1@gmail.com>
2023-01-17 14:58:17 -0500

    added drop down menu with all the states

--- index.js
+++ index.js
@@ -1,37 +1,80 @@
-// header scrolling effect
-$(window).on('scroll', function(){
-	if($(window).scrollTop()){
-      $('header').addClass('nav-show');
-		  
-	} 
-	else{
-		$('header').removeClass('nav-show');
-	}
-	   
-})
-
-//hamburger
-const navSlide = () => {
-	 const hamburger = document.querySelector(".hamburger");
-	 const navbar = document.querySelector(".nav-bar");
-	 const navLinks = document.querySelectorAll(".nav-bar li");
-
-     hamburger.onclick = () => {
-		
-	 navbar.classList.toggle("nav-active");
-		 
-      //Animation links
-	 navLinks.forEach((link, index) => {
-		if (link.style.animation) {
-			link.style.animation = "";
-		} else {
-			link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7+1}s`;
-		   }
-		});
-	  //hamburger animation
-	 hamburger.classList.toggle("toggle");
-    }
-	 
-	}
-
-window.onload = () => navSlide();
+const states = [
+	{ name: "Alabama", winningNumbers: "" },
+	{ name: "Alaska", winningNumbers: "" },
+	{ name: "Arizona", winningNumbers: "" },
+	{ name: "Arkansas", winningNumbers: "" },
+	{ name: "California", winningNumbers: "" },
+	{ name: "Colorado", winningNumbers: "" },
+	{ name: "Connecticut", winningNumbers: "" },
+	{ name: "Delaware", winningNumbers: "" },
+	{ name: "Florida", winningNumbers: "" },
+	{ name: "Georgia", winningNumbers: "" },
+	{ name: "Hawaii", winningNumbers: "" },
+	{ name: "Idaho", winningNumbers: "" },
+	{ name: "Illinois", winningNumbers: "" },
+	{ name: "Indiana", winningNumbers: "" },
+	{ name: "Iowa", winningNumbers: "" },
+	{ name: "Kansas", winningNumbers: "" },
+	{ name: "Kentucky", winningNumbers: "" },
+	{ name: "Louisiana", winningNumbers: "" },
+	{ name: "Maine", winningNumbers: "" },
+	{ name: "Maryland", winningNumbers: "" },
+	{ name: "Massachusetts", winningNumbers: "" },
+	{ name: "Michigan", winningNumbers: "" },
+	{ name: "Minnesota", winningNumbers: "" },
+	{ name: "Mississippi", winningNumbers: "" },
+	{ name: "Missouri", winningNumbers: "" },
+	{ name: "Montana", winningNumbers: "" },
+	{ name: "Nebraska", winningNumbers: "" },
+	{ name: "Nevada", winningNumbers: "" },
+	{ name: "New Hampshire", winningNumbers: "" },
+	{ name: "New Jersey", winningNumbers: "" },
+	{ name: "New Mexico", winningNumbers: "" },
+	{ name: "New York", winningNumbers: "" },
+	{ name: "North Carolina", winningNumbers: "" },
+	{ name: "North Dakota", winningNumbers: "" },
+	{ name: "Ohio", winningNumbers: "" },
+	{ name: "Oklahoma", winningNumbers: "" },
+	{ name: "Oregon", winningNumbers: "" },
+	{ name: "Pennsylvania", winningNumbers: "" },
+	{ name: "Rhode Island", winningNumbers: "" },
+	{ name: "South Carolina", winningNumbers: "" },
+	{ name: "South Dakota", winningNumbers: "" },
+	{ name: "Tennessee", winningNumbers: "" },
+	{ name: "Texas", winningNumbers: "" },
+	{ name: "Utah", winningNumbers: "" },
+	{ name: "Vermont", winningNumbers: "" },
+	{ name: "Virginia", winningNumbers: "" },
+	{ name: "Washington", winningNumbers: "" },
+	{ name: "West Virginia", winningNumbers: "" },
+	{ name: "Wisconsin", winningNumbers: "" },
+	{ name: "Wyoming", winningNumbers: "" }
+  ];
+  
+  
+  
+  const button = document.createElement("button");
+  button.textContent = "Show states";
+  document.querySelector(".winning-numbers").appendChild(button);
+  
+  const select = document.createElement("select");
+  document.querySelector(".winning-numbers").appendChild(select);
+  select.style.display = "none";
+  
+  button.addEventListener("click", e => {
+	// Clear the previous options from the select element
+	select.innerHTML = "";
+	
+	states.forEach(state => {
+	  const option = document.createElement("option");
+	  option.value = state.winningNumbers;
+	  option.textContent = state.name;
+	  select.appendChild(option);
+	});
+	select.style.display = "block";
+  });
+  
+  select.addEventListener("change", e => {
+	alert(`Winning numbers for ${e.target.options[e.target.selectedIndex].text}: ${e.target.value}`);
+  });
+  
